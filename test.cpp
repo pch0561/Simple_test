0668750790ac97220b8af432a8c68a7569d55b6a
pch0561 <chanho9451@gmail.com>
2018-12-30 17:22:17 +0900

    Chnanges to be committed: test.cpp (new file)

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int a=1,b=3;
+
+    for(int i=0;i<10;i++)
+        a++;
+    
+    cout << a <<" "<< b<< endl;
+
+    return 0;
+}
